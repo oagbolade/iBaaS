bc5681018d6792ff3206924a97f7422f77f1db7a
Oladayo Agbolade <dayoagbolade@gmail.com>
2023-07-27 03:18:14 +0100

    Implemented action menu

---
+++ components/Table/ActionMenu.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import MenuItem from '@mui/material/MenuItem';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import RemoveRedEyeOutlinedIcon from '@mui/icons-material/RemoveRedEyeOutlined';
+import EditOutlinedIcon from '@mui/icons-material/EditOutlined';
+import { TableMenuButton } from '@/components/Buttons';
+import { StyledMenu } from './StyledMenu';
+
+export const ActionMenu = () => {
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <Box
+      sx={{
+        padding: '25px',
+      }}
+    >
+      <Button onClick={handleClick}>
+        <MoreVertIcon
+          sx={{
+            color: 'black',
+          }}
+        />
+      </Button>
+      <StyledMenu anchorEl={anchorEl} open={open} onClose={handleClose}>
+        <MenuItem onClick={handleClose}>
+          <TableMenuButton buttonTitle='View Profile' icon={<RemoveRedEyeOutlinedIcon />} /> 
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+          <TableMenuButton buttonTitle='Edit' icon={<EditOutlinedIcon />} /> 
+        </MenuItem>
+      </StyledMenu>
+    </Box>
+  );
+};
