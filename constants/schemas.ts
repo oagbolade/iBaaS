dd08e6e36b3bc26889a915e480279f1300095b19
Oladayo Agbolade <dayoagbolade@gmail.com>
2023-08-07 05:50:48 +0100

    removed ewmail

--- constants/schemas.ts
+++ constants/schemas.ts
@@ -1,7 +1,6 @@
 import * as Yup from 'yup';
 
 export const user = Yup.object({
-    email: Yup.string().required('Required'),
     staffId: Yup.string().required('Required'),
     staffName: Yup.string().required('Required'),
     branch: Yup.string().required('Required'),
