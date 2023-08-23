81e69e30ec0f99e994498b037876ee1917889415
Oladayo.Agbolade <oladayo.agbolade@interswitchng.com>
2023-08-23 13:26:09 +0200

    Revert changes

--- utils/handleRedirect.ts
+++ utils/handleRedirect.ts
@@ -2,7 +2,7 @@ import { AppRouterInstance } from 'next/dist/shared/lib/app-router-context';
 
 export const handleRedirect = (
   router: AppRouterInstance | string[],
-  link?: string | undefined
+  link: string
 ) => {
   return router.push(link);
 };
