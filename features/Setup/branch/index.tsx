999c4015ce85f59bc728f124a472b7685a58aecc
Oladayo.Agbolade <oladayo.agbolade@interswitchng.com>
2023-08-24 22:32:20 +0200

    Added esling and fixed redirect issues

--- features/Setup/branch/index.tsx
+++ features/Setup/branch/index.tsx
@@ -4,6 +4,7 @@ import { StarterSection } from '@/components/Shared/StarterSection';
 export const ManageBranch = () => {
   return (
     <StarterSection
+      redirectLink="/setup/branch/steps"
       isSetup
       title="There are no listed <b>“Branch”</b>.Add new branch to get started"
       buttonTitle="Add new branch"
