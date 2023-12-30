7633c54a92728fa8b4fe84b5e58d8bb98957cab4
Oladayo.Agbolade <oladayo.agbolade@interswitchng.com>
2023-12-31 00:01:26 +0100

    Made audit trail responsive

--- features/Report/AuditTrail/FilterSection.tsx
+++ features/Report/AuditTrail/FilterSection.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Box, Typography, Stack } from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search';
-import styled from 'styled-components';
+import { inputFields } from './style';
 import { allBranchesStyle } from '@/features/Report/Overview/styles';
 import { TextInput } from '@/components/FormikFields';
 import colors from '@/assets/colors';
@@ -16,12 +16,16 @@ import {
   branchOptions,
   selectButton,
 } from '@/features/Report/CustomReport/ChartAccount/FilterSection';
-import { inputFields } from './style';
+import { useSetDirection } from '@/utils/useSetDirection';
+import { useCurrentBreakpoint } from '@/utils';
 
 export const FilterSection = () => {
+  const { setDirection } = useSetDirection();
+  const { isMobile, setWidth } = useCurrentBreakpoint();
+
   return (
     <Box>
-      <Stack direction="row">
+      <Stack direction={setDirection()}>
         <Wrapper>
           <Typography sx={labelTypography}>Module</Typography>
           <ActionButtonWithPopper
@@ -47,27 +51,32 @@ export const FilterSection = () => {
           />
         </Wrapper>
         <Wrapper>
-          <Typography sx={labelTypography}>Action</Typography>
-          <ActionButtonWithPopper
-            searchGroupVariant="BasicSearchGroup"
-            options={branchOptions}
-            customStyle={{ ...allBranchesStyle, ...selectButton }}
-            icon={
-              <ChevronDown
-                color={`${colors.Heading}`}
-                props={{ width: '12px', height: '12px' }}
-              />
-            }
-            iconPosition="end"
-            buttonTitle="Select"
-          />
+          <Box mt={1}>
+            <Typography sx={labelTypography}>Action</Typography>
+            <ActionButtonWithPopper
+              searchGroupVariant="BasicSearchGroup"
+              options={branchOptions}
+              customStyle={{ ...allBranchesStyle, ...selectButton }}
+              icon={
+                <ChevronDown
+                  color={`${colors.Heading}`}
+                  props={{ width: '12px', height: '12px' }}
+                />
+              }
+              iconPosition="end"
+              buttonTitle="Select"
+            />
+          </Box>
         </Wrapper>
         <Box mt={4.5} mr={4}>
           <TextInput
             name="Search"
             placeholder="Search"
             icon={<SearchIcon />}
-            customStyle={{ ...inputFields }}
+            customStyle={{
+              ...inputFields,
+              width: setWidth(isMobile ? '240px' : '100%'),
+            }}
           />
         </Box>
         <Box mt={4.5}>
