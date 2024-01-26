8162f87b6e53ee6b82ba7d25ce13a5a94267b2a7
Oladayo.Agbolade <oladayo.agbolade@interswitchng.com>
2024-01-26 14:05:43 +0100

    Fix path

--- components/Revamp/TableV2/ActionMenu.tsx
+++ components/Revamp/TableV2/ActionMenu.tsx
@@ -8,7 +8,7 @@ import EditOutlinedIcon from '@mui/icons-material/EditOutlined';
 import { useRouter } from 'next/navigation';
 import { StyledMenu } from './StyledMenu';
 import { TableMenuButton } from '@/components/Buttons';
-import { AdminContext } from '@/features/Admin/AdminContext';
+import { AdminContext } from '@/features/Admin-old/AdminContext';
 import { CustomerServiceContext } from '@/features/CustomerService/CustomerServiceContext';
 
 type ActionMenuProps = {
@@ -58,30 +58,48 @@ export const ActionMenu: React.ComponentType<ActionMenuProps> = ({
           }}
         />
       </Button>
-      <StyledMenu anchorEl={anchorEl} open={open} onClose={() => {return handleClose(null);}}>
+      <StyledMenu
+        anchorEl={anchorEl}
+        open={open}
+        onClose={() => {
+          return handleClose(null);
+        }}
+      >
         {useDefault ? (
           <div>
-            <MenuItem onClick={() => {return handleClose(null);}}>
+            <MenuItem
+              onClick={() => {
+                return handleClose(null);
+              }}
+            >
               <TableMenuButton
                 buttonTitle="View Profile"
                 icon={<RemoveRedEyeOutlinedIcon />}
               />
             </MenuItem>
-            <MenuItem onClick={() => {return handleClose(null);}}>
+            <MenuItem
+              onClick={() => {
+                return handleClose(null);
+              }}
+            >
               <TableMenuButton buttonTitle="Edit" icon={<EditOutlinedIcon />} />
             </MenuItem>
           </div>
         ) : (
-          options?.map((option) => {return (
-            <MenuItem
-              onClick={() => {return option.onClick?.() || handleClose(option.link);}}
-            >
-              <TableMenuButton
-                buttonTitle={option.buttonTitle}
-                icon={option.icon}
-              />
-            </MenuItem>
-          );})
+          options?.map((option) => {
+            return (
+              <MenuItem
+                onClick={() => {
+                  return option.onClick?.() || handleClose(option.link);
+                }}
+              >
+                <TableMenuButton
+                  buttonTitle={option.buttonTitle}
+                  icon={option.icon}
+                />
+              </MenuItem>
+            );
+          })
         )}
       </StyledMenu>
     </Box>
