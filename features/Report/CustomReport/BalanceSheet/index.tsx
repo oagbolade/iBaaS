8416a1db0be1fa083497e85d2d0127a1aeb7c9d7
Ogooluwakiitan <ogooluwaoladimeji@gmail.com>
2025-05-13 12:25:29 +0100

    designed balance sheet UI

--- features/Report/CustomReport/BalanceSheet/index.tsx
+++ features/Report/CustomReport/BalanceSheet/index.tsx
@@ -1,23 +1,28 @@
 'use client';
 import * as React from 'react';
 import { Box } from '@mui/material';
-import { ShortCardWithAccordion } from './ShortCardWithAccordion';
+import { GrandTotal, ShortCardWithAccordion } from './ShortCardWithAccordion';
 import { TopOverViewSection } from '@/features/Report/Overview/TopOverViewSection';
 import { column, data } from '@/constants/Reports/ASSETS_DATA';
+import { FilterSection } from './FilterSection';
+import { useGetBranches } from '@/api/general/useBranches';
 
 export const BalanceSheet = () => {
+  const { branches } = useGetBranches();
+
   return (
     <Box
       sx={{
         width: '100%',
-        marginTop: '70px'
+        marginTop: '70px',
       }}
     >
       <TopOverViewSection useBackButton />
+      <Box>{branches && <FilterSection branches={branches} />}</Box>
       <Box
         sx={{
           padding: '25px',
-          width: '100%'
+          width: '100%',
         }}
       >
         <ShortCardWithAccordion column={column} data={data} />
@@ -25,6 +30,7 @@ export const BalanceSheet = () => {
         <ShortCardWithAccordion column={column} data={data} />
         <ShortCardWithAccordion column={column} data={data} />
         <ShortCardWithAccordion column={column} data={data} />
+        <GrandTotal amount={'56679805321.54'} title="Grand Total" />
       </Box>
     </Box>
   );
