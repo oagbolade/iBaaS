787cbf423f020d892a4deac4b65b24b79e86b1a3
“Oladayo <oladayo.agbolade@interswitchng.com>
2025-10-05 19:17:33 +0100

    Fixed build issues

--- app/(app)/setup/product-gl/add-casa-product/page.tsx
+++ app/(app)/setup/product-gl/add-casa-product/page.tsx
@@ -1,5 +1,4 @@
 'use client';
-import dynamic from 'next/dynamic';
 import React from 'react';
 import { encryptData } from '@/utils/encryptData';
 import { useGetParams } from '@/utils/hooks/useGetParams';
