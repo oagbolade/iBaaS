b36cd16cbf082d8770c45db8b3339910f29a3c0b
Michael <124148519+OnyekwereMichael@users.noreply.github.com>
2025-11-13 12:49:18 +0100

    Reverted Changes
    Reverted Changes

--- features/Report/CustomReport/LoanOverdue/FilterSection.tsx
+++ features/Report/CustomReport/LoanOverdue/FilterSection.tsx
@@ -1,210 +1,207 @@
-'use client';
-import React, { useContext } from 'react';
-import { Box } from '@mui/material';
-import Link from 'next/link';
-import { FilterSection } from './FilterSection';
-import { MuiTableContainer, TableSingleAction } from '@/components/Table';
-import { useGetBranches } from '@/api/general/useBranches';
+import React, { ChangeEvent, useMemo, useState } from 'react';
+import { Box, Grid, Stack } from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
+import CalendarTodayOutlinedIcon from '@mui/icons-material/CalendarTodayOutlined';
+import { DateCalendar } from '@mui/x-date-pickers';
+import dayjs, { Dayjs } from 'dayjs';
+import { dateFilter, exportData, inputFields } from '../style';
+import { buttonBackgroundColor } from '../AccountEnquiry/style';
 import {
-  LoanOverdueParams,
-  useGetLoanOverdueReport
-} from '@/api/reports/useGetLoanOverdueReport';
-import { loanOverdueColumns } from '@/constants/Reports/COLUMNS';
-import { renderEmptyTableBody, StyledTableRow } from '@/components/Table/Table';
-import { IGetLoanOverdueReport } from '@/api/ResponseTypes/reports';
-import { StyledTableCell } from '@/components/Table/style';
-import { ReportModuleContext } from '@/context/ReportModuleContext';
-import { useGetAllProduct } from '@/api/setup/useProduct';
-import { DateRangePickerContext } from '@/context/DateRangePickerContext';
-import { DownloadReportContext } from '@/context/DownloadReportContext';
-import { FormSkeleton } from '@/components/Loaders';
+  FormSelectField,
+  FormSelectInput,
+  FormTextInput,
+  TextInput
+} from '@/components/FormikFields';
+import {
+  ActionButton,
+  ActionButtonWithPopper,
+  BackButton
+} from '@/components/Revamp/Buttons';
+import { IBranches } from '@/api/ResponseTypes/general';
+import { useMapSelectOptions } from '@/utils/hooks/useMapSelectOptions';
+import { LoanOverdueParams } from '@/api/reports/useGetLoanOverdueReport';
+import { IBankProducts } from '@/api/ResponseTypes/customer-service';
+import { useSetDirection } from '@/utils/hooks/useSetDirection';
+import { ExportIcon } from '@/assets/svg';
+import colors from '@/assets/colors';
+import useFormattedDates from '@/utils/hooks/useFormattedDates';
+import { useCurrentBreakpoint } from '@/utils';
 import { usePersistedSearch } from '@/utils/hooks/usePersistedSearch';
-import { useGlobalLoadingState } from '@/utils/hooks/useGlobalLoadingState';
-import moment from 'moment';
-import { formatCurrency } from '@/utils/hooks/useCurrencyFormat';
+import { Form, Formik } from 'formik';
+import { DateRangePickerContext } from '@/context/DateRangePickerContext';
 
-interface Props {
-  data: IGetLoanOverdueReport;
-}
+type Props = {
+  branches?: IBranches[];
+  onSearch: (params: LoanOverdueParams | null) => void;
+  bankproducts: IBankProducts[] | Array<any>;
+};
 
-export const LoanOverdue = () => {
-  const { isLoading: isGlobalLoading } = useGlobalLoadingState();
-  const { branches } = useGetBranches();
-  const { bankproducts } = useGetAllProduct();
-  const { dateValue } = React.useContext(DateRangePickerContext);
-  const { setExportData, setReportType } = useContext(DownloadReportContext);
+export const FilterSection = ({ branches, onSearch, bankproducts }: Props) => {
+  const { searchParams } =
+    usePersistedSearch<LoanOverdueParams>('loan-overdue');
+  const { setDirection } = useSetDirection();
+  const { dateValue, setDateValue } = React.useContext(DateRangePickerContext);
+  const endDate = dateValue[1];
+  const { setWidth } = useCurrentBreakpoint();
 
-  const {
-    searchParams,
-    setSearchParams,
-    searchActive,
-    setSearchActive,
-    page,
-    setPage
-  } = usePersistedSearch<LoanOverdueParams>('loan-overdue');
-
-  const {
-    loanOverDueList: loanOverDueData = [],
-    totalRecords,
-    isLoading
-  } = useGetLoanOverdueReport({
-    ...searchParams,
-    pageSize: 10,
-    pageNumber: page
+  const { mappedBranches, mappedBankproducts } = useMapSelectOptions({
+    branches,
+    bankproducts
   });
 
-  const { loanOverDueList: downloadData = [] } = useGetLoanOverdueReport({
-    ...searchParams,
-    pageSize: 10,
-    pageNumber: page,
-    getAll: true
-  });
-
-  React.useEffect(() => {
-    if (!downloadData || downloadData?.length === 0) {
-      setExportData([]);
-      return;
-    }
-
-    if (downloadData.length > 0) {
-      const formattedExportData = downloadData.map((item) => ({
-        'Acc No': item?.accountNumber || '',
-        'Prod Code': item?.productCode || '',
-        'Loan Amount': item?.loanamount || '',
-        'Start Date': item?.startdate?.split('T')[0] || '',
-        'Maturity Date': item?.matDate?.split('T')[0] || '',
-        'Principal Outstanding': item?.principal_Outstanding || '',
-        'Intrest Outstanding': item?.interest_Outstanding || '',
-        Age: item?.age || '',
-        'Last Date': item?.lastDate?.split('T')[0] || '',
-        'Group ID': item?.groupid || '',
-        'Settlement Account': item?.settlementAcct1 || '',
-        'Current Balance': item?.currentbalance || '',
-        'Penal Intrest Outstanding': item?.penalInterest_Outstanding || '',
-        'Group Name': item?.groupname || item?.groupName2 || '',
-        Report: item?.report?.split(' ')[0] || '',
-        'Acct Name': item?.fullname || '',
-        Branch: item?.branch || '',
-        'Officer Name': item?.officerName || item?.officerName2 || '',
-        'CASA Balance': item?.casa_Balance || ''
-      }));
-
-      // Ensure no blank row or misplaced headers
-      setExportData(formattedExportData);
-      setReportType('LoanOverdueReport');
-    }
-  }, [downloadData]);
-
-  const rowsPerPage = 10;
-  const totalPages = Math.ceil((totalRecords || 0) / rowsPerPage);
-
-  const handleSearch = (params: LoanOverdueParams | null) => {
-    setSearchParams({
-      ...params,
-      reportDate: dateValue[1]?.format('YYYY-MM-DD') || ''
-    });
-    setSearchActive(true);
+  const initialValues = {
+    branch: searchParams?.branch ?? '',
+    product: searchParams?.product ?? '',
+    search: searchParams?.search ?? '',
+    reportDate: searchParams?.reportDate ?? ''
   };
 
-  const LoanOverdueAction = ({ data }: Props) => {
-    const { setLoanOverduestatedata } = useContext(ReportModuleContext);
-
-    const setData = () => {
-      setLoanOverduestatedata({
-        ...data
-      });
+  const onSubmit = async (values: any) => {
+    const params: LoanOverdueParams = {
+      branch: values.branch.toString().length > 0 ? values.branch : null,
+      product: values.product.length > 0 ? values.product : undefined,
+      search: values.search.length > 0 ? values.search : undefined,
+      reportDate: endDate?.format('YYYY-MM-DD')
     };
-
-    return (
-      <Link
-        onClick={setData}
-        href="/report/custom-report/view-loanoverdue-report"
-      >
-        <TableSingleAction actionName="View" />
-      </Link>
-    );
+    onSearch?.(params);
   };
 
   return (
-    <Box sx={{ marginTop: '60px', width: '100%' }}>
-      <Box>
-        {branches && bankproducts && (
-          <FilterSection
-            branches={branches}
-            bankproducts={bankproducts}
-            onSearch={handleSearch}
-          />
-        )}
+    <Formik
+      initialValues={initialValues}
+      enableReinitialize
+      onSubmit={(values) => onSubmit(values)}
+    >
+      {() => (
+        <Form>
+          <Box>
+            <Stack
+              sx={{
+                position: 'sticky',
+                top: '60px',
+                zIndex: 3,
+                backgroundColor: `${colors.white}`,
+                borderLeft: `1px solid ${colors.loanTitleColor}`,
+                borderBottom: `1px solid ${colors.loanTitleColor}`,
+                paddingLeft: '10px',
+                paddingRight: '10px'
+              }}
+              direction={setDirection()}
+              justifyContent="space-between"
+            >
+              <Box>
+                <Box mt={2.3}>
+                  <BackButton />
+                </Box>
+              </Box>
+              <Stack
+                mt={1}
+                direction={setDirection()}
+                spacing={2}
+                justifyContent="space-between"
+              >
+                <Box>
+                  <ActionButtonWithPopper
+                    searchGroupVariant="ExportReport"
+                    customStyle={{ ...exportData }}
+                    icon={<ExportIcon />}
+                    iconPosition="start"
+                    buttonTitle="Export Data"
+                  />
+                </Box>
 
-        {isGlobalLoading || isLoading ? (
-          <FormSkeleton noOfLoaders={5} />
-        ) : (
-          <Box sx={{ padding: '25px', width: '100%' }}>
-            <MuiTableContainer
-              columns={loanOverdueColumns}
-              data={loanOverDueData}
-              page={page}
-              setPage={setPage}
-              totalPages={totalPages}
-              totalElements={totalRecords}
-              showHeader={{
-                mainTitle: 'Loan Overdue',
-                secondaryTitle:
-                  'See a directory of all Overdue Loans on this system.',
-                hideFilterSection: true
+                <Box>
+                  <ActionButtonWithPopper
+                    searchGroupVariant="DateRangePicker"
+                    CustomDateRangePicker={
+                      <DateCalendar
+                        value={endDate}
+                        onChange={(date) => setDateValue([dateValue[0], date])}
+                      />
+                    }
+                    customStyle={{ ...dateFilter }}
+                    icon={
+                      <CalendarTodayOutlinedIcon
+                        sx={{
+                          color: `${colors.Heading}`
+                        }}
+                      />
+                    }
+                    iconPosition="end"
+                    buttonTitle={endDate?.format('YYYY-MM-DD')}
+                  />
+                </Box>
+              </Stack>
+            </Stack>
+
+            <Box
+              sx={{
+                marginTop: '30px',
+                paddingX: '24px'
               }}
             >
-              {searchActive ? (
-                loanOverDueData.map(
-                  (accountData: IGetLoanOverdueReport, index) => {
-                    return (
-                      <StyledTableRow key={index}>
-                        <StyledTableCell component="th" scope="row">
-                          {accountData?.accountNumber || 'N/A'}
-                        </StyledTableCell>
-                        <StyledTableCell component="th" scope="row">
-                          {accountData?.productCode || 'N/A'}
-                        </StyledTableCell>
-                        <StyledTableCell align="right">
-                          {NGN ${formatCurrency(accountData?.loanamount || 0)} ||
-                            'N/A'}
-                        </StyledTableCell>
-                        <StyledTableCell align="right">
-                          {moment(accountData?.startdate?.split('T')[0]).format(
-                            'YYYY-MM-DD, hh:mm:ss A'
-                          ) || 'N/A'}
-                        </StyledTableCell>
-                        <StyledTableCell align="right">
-                          {moment(accountData?.matDate?.split('T')[0]).format(
-                            'YYYY-MM-DD, hh:mm:ss A'
-                          ) || 'N/A'}
-                        </StyledTableCell>
-                        <StyledTableCell align="right">
-                          {NGN ${formatCurrency(accountData?.principal_Outstanding || 0)} ||
-                            'N/A'}
-                        </StyledTableCell>
-                        <StyledTableCell align="right">
-                          <LoanOverdueAction data={accountData} />
-                        </StyledTableCell>
-                      </StyledTableRow>
-                    );
-                  }
-                )
-              ) : (
-                <StyledTableRow>
-                  <StyledTableCell
-                    colSpan={loanOverdueColumns.length + 1}
-                    component="th"
-                    scope="row"
+              <Box>
+                <Grid container spacing={2}>
+                  <Grid item mobile={12} tablet={3}>
+                    <FormSelectField
+                      customStyle={{
+                        width: setWidth(),
+                        ...inputFields
+                      }}
+                      name="branch"
+                      options={mappedBranches}
+                      label="Branch Name"
+                      required
+                    />
+                  </Grid>
+
+                  <Grid item mobile={12} tablet={3}>
+                    <FormSelectField
+                      customStyle={{
+                        width: setWidth(),
+                        ...inputFields
+                      }}
+                      name="product"
+                      options={mappedBankproducts}
+                      label="Product"
+                    />
+                  </Grid>
+
+                  <Grid item mobile={12} tablet={4}>
+                    <FormTextInput
+                      customStyle={{
+                        width: setWidth(),
+                        ...inputFields
+                      }}
+                      icon={<SearchIcon />}
+                      name="search"
+                      placeholder="Search for Account Number"
+                      label="Search for Account Number"
+                    />{' '}
+                  </Grid>
+
+                  <Grid
+                    item
+                    mobile={12}
+                    tablet={2}
+                    sx={{ display: 'flex' }}
+                    justifyContent="flex-end"
+                    mt={{ tablet: 3.2 }}
+                    mr={{ mobile: 30, tablet: 0 }}
+                    mb={{ mobile: 6, tablet: 0 }}
                   >
-                    {renderEmptyTableBody()}
-                  </StyledTableCell>
-                </StyledTableRow>
-              )}
-            </MuiTableContainer>
+                    <ActionButton
+                      type="submit"
+                      customStyle={buttonBackgroundColor}
+                      buttonTitle="Search"
+                    />
+                  </Grid>
+                </Grid>
+              </Box>
+            </Box>
           </Box>
-        )}
-      </Box>
-    </Box>
-  );
+        </Form>
+      )}
+    </Formik>
+  );
 };
